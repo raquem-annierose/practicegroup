81c86f4c9495bf8fd812c7b4cc27a0a15ad89308
raquem-annierose <annierosesraquem@iskolarngbayan.pup.edu.ph>
2025-03-12 09:17:19 +0800

    modify dedication message for mikee from annie

--- capilitan_mikee.py
+++ capilitan_mikee.py
@@ -3,4 +3,4 @@ print("December 5, 2004")
 print("Female")
 print("mikeecapilitanccapilitan@iskolarngbayan.pup.edu.ph")
 
-print("Welcome to the team! -Annie")
+print("\n Welcome to the team! - Annie")
